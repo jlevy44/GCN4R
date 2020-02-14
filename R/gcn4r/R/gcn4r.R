0f89c9c35cff92a1b20b610f40041fb616ceee81
jlevy44 <joshualevy44@berkeley.edu>
2020-02-13 20:41:23 -0500

    Update gcn4r.R

--- R/gcn4r/R/gcn4r.R
+++ R/gcn4r/R/gcn4r.R
@@ -1,6 +1,9 @@
 library(reticulate)
 library(statnet)
 library(latentnet)
+library(wfg)
+library(igraph)
+
 
 #' Install GCN4R Python Package.
 #'
@@ -163,3 +166,22 @@ net2mat <- function (G) {
   return(G)
 }
 # add ergm, ergmm, mple
+
+
+
+sim.and.plot <- function(nv=c(32, 32, 32, 32),
+                         p.in=c(0.452, 0.452, 0.452, 0.452),
+                         p.out=0.021, p.del=0) {
+  net.simu <- network.simu(nv=nv, p.in=p.in, p.out=p.out, p.del=p.del)
+  net <- net.simu$net
+  group <- net.simu$group
+
+  V(net)$size <- 7
+  V(net)$color <- group
+  plot(net, vertex.label='')
+}
+
+to.igraph <- function(A,X){
+  return(graph_from_data_frame(A, directed = TRUE, vertices = X))
+}
+
